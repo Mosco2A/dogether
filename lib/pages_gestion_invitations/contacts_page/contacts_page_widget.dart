1509696c0451bb2823615123702b0e840e26b3bb
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-24 13:18:43 +0000

    Updating to latest FlutterFlow output.

--- lib/pages_gestion_invitations/contacts_page/contacts_page_widget.dart
+++ lib/pages_gestion_invitations/contacts_page/contacts_page_widget.dart
@@ -102,158 +102,164 @@ class _ContactsPageWidgetState extends State<ContactsPageWidget> {
             : null,
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              StreamBuilder<List<MyContactsRecord>>(
-                stream: queryMyContactsRecord(),
-                builder: (context, snapshot) {
-                  // Customize what your widget looks like when it's loading.
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: SizedBox(
-                        width: 50.0,
-                        height: 50.0,
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            FlutterFlowTheme.of(context).primary,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                StreamBuilder<List<MyContactsRecord>>(
+                  stream: queryMyContactsRecord(),
+                  builder: (context, snapshot) {
+                    // Customize what your widget looks like when it's loading.
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: SizedBox(
+                          width: 50.0,
+                          height: 50.0,
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              FlutterFlowTheme.of(context).primary,
+                            ),
                           ),
                         ),
+                      );
+                    }
+                    List<MyContactsRecord> containerMyContactsRecordList =
+                        snapshot.data!;
+
+                    return Container(
+                      width: MediaQuery.sizeOf(context).width * 0.95,
+                      height: MediaQuery.sizeOf(context).height * 0.6,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).primary,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 20.0, 0.0, 20.0),
+                            child: Builder(
+                              builder: (context) {
+                                final listeviewCP =
+                                    containerMyContactsRecordList.toList();
+
+                                return ListView.builder(
+                                  padding: EdgeInsets.zero,
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: listeviewCP.length,
+                                  itemBuilder: (context, listeviewCPIndex) {
+                                    final listeviewCPItem =
+                                        listeviewCP[listeviewCPIndex];
+                                    return Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  0.0, 5.0, 0.0, 5.0),
+                                          child: Text(
+                                            listeviewCPItem.name,
+                                            style: FlutterFlowTheme.of(context)
+                                                .labelMedium
+                                                .override(
+                                                  fontFamily: 'Inter',
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              EdgeInsetsDirectional.fromSTEB(
+                                                  0.0, 5.0, 0.0, 5.0),
+                                          child: Text(
+                                            listeviewCPItem.phone,
+                                            style: FlutterFlowTheme.of(context)
+                                                .labelMedium
+                                                .override(
+                                                  fontFamily: 'Inter',
+                                                  letterSpacing: 0.0,
+                                                ),
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     );
-                  }
-                  List<MyContactsRecord> containerMyContactsRecordList =
-                      snapshot.data!;
-
-                  return Container(
-                    width: MediaQuery.sizeOf(context).width * 0.95,
-                    height: MediaQuery.sizeOf(context).height * 0.6,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).primary,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 20.0, 0.0, 20.0),
-                          child: Builder(
-                            builder: (context) {
-                              final listeviewCP =
-                                  containerMyContactsRecordList.toList();
-
-                              return ListView.builder(
-                                padding: EdgeInsets.zero,
-                                shrinkWrap: true,
-                                scrollDirection: Axis.vertical,
-                                itemCount: listeviewCP.length,
-                                itemBuilder: (context, listeviewCPIndex) {
-                                  final listeviewCPItem =
-                                      listeviewCP[listeviewCPIndex];
-                                  return Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 5.0, 0.0, 5.0),
-                                        child: Text(
-                                          listeviewCPItem.name,
-                                          style: FlutterFlowTheme.of(context)
-                                              .labelMedium
-                                              .override(
-                                                fontFamily: 'Inter',
-                                                letterSpacing: 0.0,
-                                              ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 5.0, 0.0, 5.0),
-                                        child: Text(
-                                          listeviewCPItem.phone,
-                                          style: FlutterFlowTheme.of(context)
-                                              .labelMedium
-                                              .override(
-                                                fontFamily: 'Inter',
-                                                letterSpacing: 0.0,
-                                              ),
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              Container(
-                width: MediaQuery.sizeOf(context).width * 0.95,
-                height: MediaQuery.sizeOf(context).height * 0.12,
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).primary,
+                  },
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 10.0),
-                      child: Text(
-                        'Vous pouvez ajouter des contacts depuis votre répertoire.',
-                        style: FlutterFlowTheme.of(context).labelLarge.override(
-                              fontFamily: 'Inter',
-                              letterSpacing: 0.0,
-                            ),
-                      ),
-                    ),
-                    Align(
-                      alignment: AlignmentDirectional(0.0, 0.0),
-                      child: FFButtonWidget(
-                        onPressed: () async {
-                          await actions.listeContacts(
-                            context,
-                          );
-                        },
-                        text: 'Ajouter',
-                        options: FFButtonOptions(
-                          width: 120.0,
-                          height: 30.0,
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          iconPadding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: Color(0xFF4B39EF),
-                          textStyle:
-                              FlutterFlowTheme.of(context).titleLarge.override(
+                Container(
+                  width: MediaQuery.sizeOf(context).width * 0.95,
+                  height: MediaQuery.sizeOf(context).height * 0.12,
+                  decoration: BoxDecoration(
+                    color: FlutterFlowTheme.of(context).primary,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(
+                            0.0, 10.0, 0.0, 10.0),
+                        child: Text(
+                          'Vous pouvez ajouter des contacts depuis votre répertoire.',
+                          style:
+                              FlutterFlowTheme.of(context).labelLarge.override(
                                     fontFamily: 'Inter',
                                     letterSpacing: 0.0,
                                   ),
-                          elevation: 3.0,
-                          borderSide: BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(12.0),
                         ),
                       ),
-                    ),
-                  ],
+                      Align(
+                        alignment: AlignmentDirectional(0.0, 0.0),
+                        child: FFButtonWidget(
+                          onPressed: () async {
+                            await actions.listeContacts(
+                              context,
+                            );
+                          },
+                          text: 'Ajouter',
+                          options: FFButtonOptions(
+                            width: 120.0,
+                            height: 30.0,
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            iconPadding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 0.0),
+                            color: Color(0xFF4B39EF),
+                            textStyle: FlutterFlowTheme.of(context)
+                                .titleLarge
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                            elevation: 3.0,
+                            borderSide: BorderSide(
+                              color: Colors.transparent,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              wrapWithModel(
-                model: _model.bottomBarModel,
-                updateCallback: () => safeSetState(() {}),
-                child: BottomBarWidget(),
-              ),
-            ],
+                wrapWithModel(
+                  model: _model.bottomBarModel,
+                  updateCallback: () => safeSetState(() {}),
+                  child: BottomBarWidget(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
