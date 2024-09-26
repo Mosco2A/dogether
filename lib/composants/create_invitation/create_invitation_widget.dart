f69d64e801b8399a4a7d0244459eee8dff3dee55
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-26 16:06:43 +0000

    Updating to latest FlutterFlow output.

--- lib/composants/create_invitation/create_invitation_widget.dart
+++ lib/composants/create_invitation/create_invitation_widget.dart
@@ -1202,6 +1202,20 @@ class _CreateInvitationWidgetState extends State<CreateInvitationWidget> {
                                                                                   isEqualTo: listeviewCreateItem.phone,
                                                                                 ),
                                                                               );
+                                                                              await showDialog(
+                                                                                context: context,
+                                                                                builder: (alertDialogContext) {
+                                                                                  return AlertDialog(
+                                                                                    title: const Text('ICI'),
+                                                                                    actions: [
+                                                                                      TextButton(
+                                                                                        onPressed: () => Navigator.pop(alertDialogContext),
+                                                                                        child: const Text('Ok'),
+                                                                                      ),
+                                                                                    ],
+                                                                                  );
+                                                                                },
+                                                                              );
                                                                               if (_model.phoneExist! > 0) {
                                                                                 FFAppState().updateCheckboxListAtIndex(
                                                                                   listeviewCreateIndex,
