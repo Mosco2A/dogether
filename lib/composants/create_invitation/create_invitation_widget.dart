c27f8844efdb2aa82430772853d4a9dd4ae1c088
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-27 03:54:51 +0000

    Updating to latest FlutterFlow output.

--- lib/composants/create_invitation/create_invitation_widget.dart
+++ lib/composants/create_invitation/create_invitation_widget.dart
@@ -1288,7 +1288,7 @@ class _CreateInvitationWidgetState extends State<CreateInvitationWidget> {
                                                               MainAxisSize.max,
                                                           mainAxisAlignment:
                                                               MainAxisAlignment
-                                                                  .spaceBetween,
+                                                                  .start,
                                                           children: [
                                                             Icon(
                                                               Icons
