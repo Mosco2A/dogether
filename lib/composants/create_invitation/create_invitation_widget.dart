7882fc466b80dc7c809a57c8d91a7fef15b74b98
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-02 12:38:38 +0000

    Updating to latest FlutterFlow output.

--- lib/composants/create_invitation/create_invitation_widget.dart
+++ lib/composants/create_invitation/create_invitation_widget.dart
@@ -1789,6 +1789,21 @@ class _CreateInvitationWidgetState extends State<CreateInvitationWidget> {
                                   ),
                                   invitationsEmisesRecordReference);
                           _shouldSetState = true;
+                          await showDialog(
+                            context: context,
+                            builder: (alertDialogContext) {
+                              return AlertDialog(
+                                title: Text('ICI'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(alertDialogContext),
+                                    child: Text('Ok'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
 
                           await widget!.selectedInvitation!.reference
                               .update(createInvitationsEmisesRecordData());
